427686b7e5758aac9ad92d5a36558fe15810ebcc
Motsiiaka Denys <denzeml313@gmail.com>
2024-09-16 19:46:58 +0300

    LR2, LR3, LR1 like C++ConsoleApp

---
+++ LR2/LR2/LR2.cpp
@@ -0,0 +1,38 @@
+﻿// LR2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<cmath>
+
+int function(int x) {
+    if (x <= 0) {
+        return x * x - sin(x);
+    }
+    if (x > 0) {
+
+        return pow(x, 1.0 / 2) + cos(x);
+    }
+}
+
+bool function2(double x, double y) {
+    if ((x >= -1.0 && x <= 0.0) && (y >= 0.0 && y <= 1)) {
+        return true;
+    }
+    if ((x >= 0 && x <= 1) && (y >= -1 && y <= 0)) {
+        return true;
+    }
+    return false;
+    
+}
+
+int main()
+{
+    
+    int x;
+    std::cin >> x;
+    std::cout << function(x) <<std::endl;
+    
+    //std::cout << function2(1, -0.5) << std::endl;
+}
+
+
---
+++ LR3/LR3/LR3.cpp
@@ -0,0 +1,39 @@
+﻿// LR3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int functionF(int i) {
+    int first = i % 1000;
+    first /= 100;
+
+    int second = i % 100;
+    second /= 10;
+
+    int third = i % 10;
+    third /= 1;
+
+    int c = 0;
+
+    if (first >2 && first %2 ==1)
+    {
+        c += first;
+    }
+    if (second > 2 && second % 2 == 1)
+    {
+        c += second;
+    }
+    if (third > 2 && third % 2 != 1)///sdfasfasf
+    {
+        c += third;
+    }
+
+
+    return c;
+}
+
+int main()
+{
+    std::cout << functionF(321) <<std::endl;    
+   //std::cout << ((2 % 2) != 1);
+}
